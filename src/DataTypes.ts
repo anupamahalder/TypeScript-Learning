59510611632792415ee0b5fe152836e5304838c6
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-04-12 12:45:19 +0530

    Create DataTypes.ts

---
+++ src/DataTypes.ts
@@ -0,0 +1,18 @@
+/**
+ * Data types that introduces in TypeScript:
+ * 1. any
+ * 2. unknown
+ * 3. never
+ * 4. enum
+ * 5. tuple
+ */
+
+/**
+ * NOTE: 
+ * 1. If we initialize a variable then typescript understand the datatype of that variable so if we donot write the datatype but intialized we can see the datatype whenever we hover on the datatype
+ * 2. If we donot initialize and as well as not mentioning the datatypes the typescript will label it with any datatype.
+ */
+let sales: number = 123_456_789;
+let corse: string = "TypeScript";
+let is_Published: boolean = true;
+let level;
